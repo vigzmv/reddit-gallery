3dbb5dd1eb6acbff641854a263f03167b7fec7c8
vigzmv <vigzmv@outlook.com>
2018-06-20 01:47:39 +0530

    Add Feed & feed-items component, RedditApi

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import redditSearch from './services/reddit';
 const searchPanel = document.querySelector('search-panel');
 
 searchPanel.addEventListener('search', async e => {
-  await redditSearch(e.detail);
+  const posts = await redditSearch(e.detail);
+  const feed = document.querySelector('.feed');
+  feed.innerHTML = '';
+
+  posts.forEach(({ title, image, permalink }) => {
+    const feedItem = document.createElement('feed-item');
+    feedItem.title = title;
+    feedItem.image = image;
+    feedItem.permalink = `https://www.reddit.com${permalink}`;
+    feed.appendChild(feedItem);
+  });
 });
--- src/services/reddit.js
+++ src/services/reddit.js
@@ -1,8 +1,11 @@
 const REDDIT_BASE_URL = 'https://www.reddit.com';
 
 const redditSearch = ({ subreddit, searchTag, timePeriod }) => {
-  const url = new URL(`${REDDIT_BASE_URL}/r/${subreddit}/${searchTag ? `search` : `top`}.json`);
-  const queryParams = {
+  // if search terms is given, do a search, else give top
+  const searchType = searchTag ? `search` : `top`;
+
+  const url = new URL(`${REDDIT_BASE_URL}/r/${subreddit}/${searchType}.json`);
+  const searchParams = {
     sort: 'top',
     restrict_sr: 'on',
     type: 'image',
@@ -10,9 +13,17 @@ const redditSearch = ({ subreddit, searchTag, timePeriod }) => {
     q: searchTag,
     t: timePeriod,
   };
-  url.search = new URLSearchParams(queryParams);
+  url.search = new URLSearchParams(searchParams);
 
-  return fetch(url).then(res => res.json());
+  return fetch(url)
+    .then(res => res.json())
+    .then(res =>
+      res.data.children.map(({ data }) => ({
+        title: data.title,
+        image: data.thumbnail,
+        permalink: data.permalink,
+      })),
+    );
 };
 
 export default redditSearch;
