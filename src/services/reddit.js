55a789dd64f3ad93d423fd77baa5f00683033a38
vigzmv <vigzmv@outlook.com>
2018-06-21 02:26:22 +0530

    Add multiple subreddits search

--- src/services/reddit.js
+++ src/services/reddit.js
@@ -1,6 +1,9 @@
 const REDDIT_BASE_URL = 'https://www.reddit.com';
 
-const redditSearch = ({ subreddit, searchTag, timePeriod }, after = false) => {
+const getUrlWithQueries = (
+  { subreddit, searchTag, timePeriod, after },
+  manyUrls = false,
+) => {
   // if search terms is given, do a search, else give top
   const searchType = searchTag ? `search` : `top`;
 
@@ -9,7 +12,7 @@ const redditSearch = ({ subreddit, searchTag, timePeriod }, after = false) => {
     sort: 'top',
     restrict_sr: 'on',
     type: 'image',
-    limit: '20',
+    limit: manyUrls ? '10' : '20',
     t: timePeriod,
   };
 
@@ -23,16 +26,56 @@ const redditSearch = ({ subreddit, searchTag, timePeriod }, after = false) => {
 
   url.search = new URLSearchParams(searchParams);
 
-  return fetch(url)
-    .then(res => res.json())
-    .then(res => ({
-      after: res.data.after,
-      posts: res.data.children.map(({ data }) => ({
-        title: data.title,
-        image: data.thumbnail,
-        link: data.permalink,
+  return url;
+};
+
+const redditSearch = (searchParams, after) => {
+  let urls = [];
+  const subreddits = searchParams.subreddit.split(',');
+  const manyUrls = subreddits.length > 1;
+
+  subreddits.forEach((subreddit, i) => {
+    urls.push(
+      getUrlWithQueries(
+        {
+          after: after && after[i],
+          subreddit: subreddit.trim(),
+          searchTag: searchParams.searchTag,
+          timePeriod: searchParams.timePeriod,
+        },
+        manyUrls,
+      ),
+    );
+  });
+
+  const requests = urls.map(url =>
+    fetch(url)
+      .then(res => res.json())
+      .then(({ data }) => ({
+        after: (data && data.after) || false,
+        posts:
+          (data &&
+            data.children &&
+            data.children.map(({ data }) => ({
+              title: data.title,
+              image: data.thumbnail,
+              link: data.permalink,
+            }))) ||
+          [],
       })),
-    }));
+  );
+
+  return Promise.all(requests).then(res =>
+    res.reduce(
+      (result, item) => {
+        result.after.push(item.after);
+        item.posts.forEach(post => result.posts.push(post));
+
+        return result;
+      },
+      { posts: [], after: [] },
+    ),
+  );
 };
 
 export default redditSearch;
