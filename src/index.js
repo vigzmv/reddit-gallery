4bf0c6ea3a9da4b7b12ce8eb31619f905abb9400
vigzmv <vigzmv@outlook.com>
2018-06-20 22:09:21 +0530

    Add response cache for searches with 5 min expiry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import redditSearch from './services/reddit';
 
-const searchPanel = document.querySelector('search-panel');
+const FIVE_MINUTES = 5 * 1000;
 const feed = document.querySelector('.feed');
+const searchPanel = document.querySelector('search-panel');
 
 let redditResponse;
 let searchParams;
@@ -10,6 +11,7 @@ let fetchingResults = false;
 const setPosts = posts => {
   posts.forEach(({ title, image, link }) => {
     const feedItem = document.createElement('feed-item');
+
     feedItem.title = title;
     feedItem.image = image;
     feedItem.link = `https://www.reddit.com${link}`;
@@ -17,10 +19,40 @@ const setPosts = posts => {
   });
 };
 
+const setLocalCache = (key, value) => {
+  localStorage.setItem(JSON.stringify(key), JSON.stringify(value));
+  localStorage.setItem(`${JSON.stringify(key)}-timestamp`, Date.now());
+};
+
+const clearLocalCache = key => {
+  localStorage.removeItem(JSON.stringify(key));
+  localStorage.removeItem(`${JSON.stringify(key)}-timestamp`);
+};
+
 searchPanel.addEventListener('search', async e => {
   Pace.restart();
   searchParams = e.detail;
-  redditResponse = await redditSearch(searchParams);
+
+  const cachedResponse = localStorage.getItem(JSON.stringify(searchParams));
+
+  if (cachedResponse) {
+    const timestamp = JSON.parse(
+      localStorage.getItem(`${JSON.stringify(searchParams)}-timestamp`),
+    );
+
+    if (new Date(timestamp + FIVE_MINUTES) > new Date()) {
+      redditResponse = JSON.parse(cachedResponse);
+    } else {
+      clearLocalCache();
+
+      redditResponse = await redditSearch(searchParams);
+      setLocalCache(searchParams, redditResponse);
+    }
+  } else {
+    redditResponse = await redditSearch(searchParams);
+    setLocalCache(searchParams, redditResponse);
+  }
+
   feed.innerHTML = '';
   setPosts(redditResponse.posts);
 });
@@ -32,6 +64,7 @@ window.addEventListener('scroll', async function() {
   ) {
     fetchingResults = true;
     Pace.restart();
+
     redditResponse = await redditSearch(searchParams, redditResponse.after);
     setPosts(redditResponse.posts);
     fetchingResults = false;
